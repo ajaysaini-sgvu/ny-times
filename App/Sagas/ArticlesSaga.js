3bd7baf80d22aa5dd68d4441120ffb107c3a5c26
Ajay Saini <ajay.saini@publicissapient.com>
2019-12-03 22:10:14 +0530

    Fixes

--- App/Sagas/ArticlesSaga.js
+++ App/Sagas/ArticlesSaga.js
@@ -2,12 +2,6 @@ import { put, call } from 'redux-saga/effects'
 import ArticlesActions from 'App/Stores/Articles/Actions'
 import { articlesService } from 'App/Services/ArticlesService'
 
-/**
- * A saga can contain multiple functions.
- *
- * This example saga contains only one to fetch articles.
- * Feel free to remove it.
- */
 export function* fetchArticles() {
   // Dispatch a redux action using `put()`
   // @see https://redux-saga.js.org/docs/basics/DispatchingActions.html
